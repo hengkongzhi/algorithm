82463b1886410268a446c9ecd309a633997ae906
cyang <1522207896@qq.com>
2021-10-01 00:36:37 +0800

    1001 提交O1的反转单词的方法

--- src/151reversewords.cpp
+++ src/151reversewords.cpp
@@ -52,11 +52,58 @@ public:
         return tmp;
 
     }
+    string reverseWords1(string s)
+    {
+        int fast = 0;
+        int slow = 0;
+        while (s[fast++] == ' ');
+        fast -= 1;
+        for (; fast < s.size(); fast++)
+        {
+            if (fast > 0 && s[fast] == s[fast - 1] && s[fast] == ' ')
+            {
+                continue;
+            }
+            s[slow++] = s[fast];
+        }
+        if (s[slow - 1] == ' ')
+        {
+            s.resize(slow - 1);
+        }
+        else
+        {
+            s.resize(slow);
+        }
+        reverse(s.begin(), s.end());
+        int start = 0;
+        int end = 0;
+        bool on = true;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (on && s[i] != ' ')
+            {
+                start = i;
+                on = false;
+            }
+            if (s[i] == ' ')
+            {
+                end = i;
+                reverse(s.begin() + start, s.begin() + end);
+                on = true;
+            }
+            if (i == s.size() - 1)
+            {
+                end = i + 1;
+                reverse(s.begin() + start, s.begin() + end);
+            }
+        }
+        return s;
+    }
 };
 int main()
 {
     string s = "  Bob    Loves  Alice   ";
     Solution solve;
-    cout << solve.reverseWords(s) << endl;
+    cout << solve.reverseWords1(s) << endl;
     return 0;
 }
