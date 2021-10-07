1ef528e966eaca54439dc44a06105b4c4eb7b123
cyang <1522207896@qq.com>
2021-10-07 23:45:19 +0800

    1007 添加KMP算法

--- src/28strStr.cpp
+++ src/28strStr.cpp
@@ -34,12 +34,100 @@ public:
         }
         return -1;
     }
+    void getNext(int* next, string& s)
+    {
+        int j = -1;
+        next[0] = j;
+        for (int i = 1; i < s.size(); i++)
+        {
+            while (j >= 0 && s[i] != s[j + 1])
+            {
+                j = next[j];
+            }
+            if (s[i] == s[j + 1])
+            {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+    int strStr1(string haystack, string needle)
+    {
+        const int sz = needle.size();
+        if (sz == 0)
+        {
+            return 0;
+        }
+        int next[sz] = {0};
+        getNext(next, needle);
+        int j = -1;
+        for (int i = 0; i < haystack.size(); i++)
+        {
+            while (j >= 0 && haystack[i] != needle[j + 1])
+            {
+                j = next[j];
+            }
+            if (haystack[i] == needle[j + 1])
+            {
+                j++;
+            }
+            if (j == needle.size() - 1)
+            {
+                return (i - needle.size() + 1);
+            }
+        }
+        return -1;
+    }
+    void getNext1(int* next, string& s)
+    {
+        int j = 0;
+        next[0] = j;
+        for (int i = 1; i < s.size(); i++)
+        {
+            while (j > 0 && s[i] != s[j])
+            {
+                j = next[j - 1];
+            }
+            if (s[i] == s[j])
+            {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+    int strStr2(string haystack, string needle)
+    {
+        const int sz = needle.size();
+        if (sz == 0)
+        {
+            return 0;
+        }
+        int next[sz] = {0};
+        getNext1(next, needle);
+        int j = 0;
+        for (int i = 0; i < haystack.size(); i++)
+        {
+            while (j > 0 && haystack[i] != needle[j])
+            {
+                j = next[j - 1];
+            }
+            if (haystack[i] == needle[j])
+            {
+                j++;
+            }
+            if (j == needle.size())
+            {
+                return (i - needle.size() + 1);
+            }
+        }
+        return -1;
+    }
 };
 int main()
 {
-    string s1 = "aaaaa";
-    string s2 = "";
+    string s1 = "aabaa";
+    string s2 = "baa";
     Solution solve;
-    cout << solve.strStr(s1, s2) << endl;
+    cout << solve.strStr2(s1, s2) << endl;
     return 0;
 }
