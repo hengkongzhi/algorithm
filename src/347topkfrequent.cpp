2f67126ec335803d1a9704903509774c1b694761
cyang <1522207896@qq.com>
2021-10-19 23:54:57 +0800

    1019 使用优先级队列解决topK问题

--- src/347topkfrequent.cpp
+++ src/347topkfrequent.cpp
@@ -5,49 +5,68 @@
 #include <queue>
 #include <deque>
 #include <algorithm>
+#include <map>
 #include <stdlib.h>
 using namespace std;
 class Solution 
 {
 public:
+    class MyFunc
+    {
+    public:
+        bool operator()(pair<int, int>& a, pair<int, int>& b)
+        {
+            return a.second > b.second;
+        }
+
+    };
     vector<int> topKFrequent(vector<int>& nums, int k) 
     {
         vector<int> myV;
+        map<int, int> myMap;
         vector<pair<int, int>> myPV;
-        queue<int> myQ;
-        sort(nums.begin(), nums.end());
-        int count = 0;
-        int pre = 0;
-        for (auto num : nums)
+        for (int i = 0; i < nums.size(); i++)
         {
-            if (myQ.empty())
-            {
-                myQ.push(num);
-                pre = num;
-                count++;
-            }
-            else
-            {
-                if (num == myQ.front())
-                {
-                    count++;
-                }
-                else
-                {
-                    myPV.push_back(pair<int, int>(pre, count));
-                    myQ.pop();
-                    myQ.push(num);
-                    pre = num;
-                    count = 1;
-                }
-            }
+            myMap[nums[i]]++;
         }
-        myPV.push_back(pair<int, int>(pre, count));
+        
+        for (auto iter = myMap.begin(); iter != myMap.end(); iter++)
+        {
+            myPV.push_back(*iter);
+        }
 
         sort(myPV.begin(), myPV.end(), [](pair<int, int>& p1, pair<int, int>& p2){return p1.second > p2.second;});
         for (int i = 0; i < k; i++)
         {
             myV.push_back(myPV[i].first);
+        }
+        return myV;
+
+    }
+    vector<int> topKFrequent1(vector<int>& nums, int k) 
+    {
+        vector<int> myV(k);
+        map<int, int> myMap;
+        int idx = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            myMap[nums[i]]++;
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, MyFunc> myPri;
+        for (auto iter = myMap.begin(); iter != myMap.end(); iter++)
+        {
+            myPri.push(*iter);
+            idx++;
+            if (idx > k)
+            {
+                myPri.pop();
+                idx--;
+            }
+        }
+        for (int i = k - 1; i >= 0; i--)
+        {
+            myV[i] = myPri.top().first;
+            myPri.pop();
         }
         return myV;
 
@@ -65,6 +84,6 @@ int main()
 {
     Solution sol;
     vector<int> nums{1, 2};
-    sol.printVec(sol.topKFrequent(nums, 2));
+    sol.printVec(sol.topKFrequent1(nums, 2));
     return 0;
 }
