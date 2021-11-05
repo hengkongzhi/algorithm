dafd58b910b02e516ce1dd9ae6dbe5817ff8d6c3
cyang <1522207896@qq.com>
2021-11-06 00:34:09 +0800

    1106 添加求左叶子节点之和

---
+++ src/404sumofleftleaves.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <stdlib.h>
+#include <math.h>
+#include <sstream>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr)
+    {
+    }
+    TreeNode(int num) : val(num), left(nullptr), right(nullptr)
+    {
+    }
+    TreeNode(int num, TreeNode* l, TreeNode* r) : val(0), left(l), right(r)
+    {
+    }
+
+};
+class Solution 
+{
+public:
+    void getVec(TreeNode* cur, bool isLeft, vector<int>& myV)
+    {
+        if (cur == nullptr)
+        {
+            return;
+        }
+        if (cur->left == nullptr && cur->right == nullptr && isLeft)
+        {
+            myV.push_back(cur->val);
+        }
+        if (cur->left)
+        {
+            getVec(cur->left, true, myV);
+        }
+        if (cur->right)
+        {
+            getVec(cur->right, false, myV);
+        }
+    }
+    int sumOfLeftLeaves(TreeNode* root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        vector<int> myV;
+        getVec(root, false, myV);
+        int sum = 0;
+        for (auto& val : myV)
+        {
+            sum += val;
+        }
+        return sum;
+    }
+
+};
+int main()
+{
+    Solution sol;
+    TreeNode* root = new TreeNode(1);
+    TreeNode* f1 = new TreeNode(2);
+    TreeNode* f2 = new TreeNode(3);
+    TreeNode* f4 = new TreeNode(4);
+    TreeNode* f6 = new TreeNode(5);
+    root->left = f1;
+    root->right = f2;
+    f1->left = f4;
+    f2->right = f6;
+    cout << sol.sumOfLeftLeaves(root) << endl;
+    return 0;
+}
