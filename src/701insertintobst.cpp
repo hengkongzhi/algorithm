3f3e8f0a42c4e8d6dc9bc32a2f0a1c0c7a3185d8
cyang <1522207896@qq.com>
2021-11-22 00:58:15 +0800

    1122 二叉搜索树插入

---
+++ src/701insertintobst.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <stdlib.h>
+#include <math.h>
+#include <sstream>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr)
+    {
+    }
+    TreeNode(int num) : val(num), left(nullptr), right(nullptr)
+    {
+    }
+    TreeNode(int num, TreeNode* l, TreeNode* r) : val(0), left(l), right(r)
+    {
+    }
+
+};
+class Solution 
+{
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val)
+    {
+        TreeNode* pVal = new TreeNode(val);
+        if (root == nullptr)
+        {
+            return pVal;
+        }
+        TreeNode* cur = root;
+        while (cur)
+        {
+            if (val > cur->val)
+            {
+                if (cur->right)
+                {
+                    cur = cur->right;
+                }
+                else
+                {
+                    cur->right = pVal;
+                    break;
+                }
+            }
+            if (val < cur->val)
+            {
+                if (cur->left)
+                {
+                    cur = cur->left;
+                }
+                else
+                {
+                    cur->left = pVal;
+                    break;
+                } 
+            }
+        }
+        return root;
+    }
+    void midOrder(TreeNode* root)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+        if (root->left)
+        {
+            midOrder(root->left);
+        }
+        cout << root->val << " ";
+        if (root->right)
+        {
+            midOrder(root->right);
+        }
+    }
+
+};
+int main()
+{
+    Solution sol;
+    TreeNode* root = new TreeNode(3);
+    TreeNode* f1 = new TreeNode(2);
+    TreeNode* f2 = new TreeNode(4);
+    TreeNode* f4 = new TreeNode(1);
+    TreeNode* f6 = new TreeNode(5);
+    root->left = f1;
+    root->right = f2;
+    f1->left = f4;
+    f2->right = f6;
+    sol.midOrder(sol.insertIntoBST(root, 0));
+    cout << endl;
+    return 0;
+}
