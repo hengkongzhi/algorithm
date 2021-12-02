1ba604e4ed9e7900546b37fd6dfa1a740e95212e
cyang <1522207896@qq.com>
2021-12-03 00:03:24 +0800

    1203 优化组合求和问题

--- src/216combination.cpp
+++ src/216combination.cpp
@@ -46,15 +46,33 @@ public:
             }
         }
     }
+
+    void com1(int k, int n, int start)
+    {
+        if (path.size() == k && n == 0)
+        {
+            tmpRet.push_back(path);
+            return;
+        }
+        if (path.size() == k)
+        {
+            return;
+        }
+        for (int i = start; i <= 9 - k + 1 + path.size(); i++)
+        {
+            path.push_back(i);
+            com1(k, n - i, i + 1);
+            path.pop_back();
+        }
+    }
     vector<vector<int>> combinationSum3(int k, int n)
     {
-        com(k, 1);
-        findRet(n);
-        return ret;
+        com1(k, n, 1);
+        return tmpRet;
     }
     void vecPrint()
     {
-        for (auto vec : ret)
+        for (auto vec : tmpRet)
         {
             for (auto data : vec)
             {
