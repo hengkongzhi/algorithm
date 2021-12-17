a24d9d7af42bd8f17294a626ef2d1217129ee88d
cyang <1522207896@qq.com>
2021-12-18 01:23:24 +0800

    1218 重新安排行程

--- src/332finditinerary.cpp
+++ src/332finditinerary.cpp
@@ -6,6 +6,8 @@
 #include <deque>
 #include <algorithm>
 #include <unordered_set>
+#include <unordered_map>
+#include <map>
 #include <stdlib.h>
 #include <math.h>
 #include <sstream>
@@ -39,11 +41,11 @@ public:
     vector<string> path;
     int count = 0;
 
-    void back(string findStr, vector<vector<string>>& tickets, vector<bool>& use)
+    bool back(string findStr, vector<vector<string>>& tickets, vector<bool>& use)
     {
         if (count == use.size())
         {
-            return;
+            return true;
         }
         for (int i = 0; i < tickets.size(); i++)
         {
@@ -64,10 +66,9 @@ public:
                 {
                     path.push_back(findStr);
                 }
-                back(tickets[i][1], tickets, use);
-                if (count == use.size())
+                if (back(tickets[i][1], tickets, use))
                 {
-                    return;
+                    return true;
                 }
                 else
                 {
@@ -76,12 +77,49 @@ public:
                 use[i] = false;
                 count--;
             }
+            return false;
         }
     }
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         sort(tickets.begin(), tickets.end(), Compare());
         vector<bool> use(tickets.size(), false);
         back("JFK", tickets, use);
+        return path;
+    }
+
+    unordered_map<string, map<string, int>> tickets;
+    bool goBack(int size)
+    {
+       if (count == size)
+       {
+           return true;
+       } 
+       for (auto& val : tickets[path.back()])
+       {
+           if (val.second > 0)
+           {
+               path.push_back(val.first);
+               val.second--;
+               count++;
+               if (goBack(size))
+               {
+                   return true;
+               }
+               count--;
+               val.second++;
+               path.pop_back();
+           }
+       }
+       return false;
+    }
+    vector<string> findItinerary1(vector<vector<string>>& tic)
+    {
+        for (auto vec : tic)
+        {
+            tickets[vec[0]][vec[1]]++;
+        }
+        path.push_back("JFK");
+        goBack(tic.size());
         return path;
     }
 
@@ -98,8 +136,8 @@ public:
 int main()
 {
     Solution sol;
-    vector<vector<string>> tickets = {{"AUA","EZE"},{"JFK","AUA"},{"EZE","ANU"},{"AXA","JFK"},{"AUA","JFK"},{"ADL","TIA"},{"AUA","AXA"},{"ADL","AUA"},{"AUA","AXA"},{"AXA","TIA"},{"EZE","AXA"},{"AXA","AUA"},{"ANU","JFK"},{"ADL","EZE"},{"ADL","ANU"},{"EZE","ADL"},{"AXA","AUA"},{"JFK","EZE"},{"AUA","JFK"},{"AUA","JFK"},{"AXA","JFK"},{"ADL","EZE"},{"AUA","EZE"},{"AUA","JFK"},{"AUA","JFK"},{"JFK","AXA"},{"TIA","AXA"},{"JFK","ADL"},{"ADL","AXA"},{"AXA","AUA"},{"AUA","AXA"},{"JFK","AXA"},{"TIA","JFK"},{"ANU","AXA"},{"JFK","AXA"},{"EZE","AXA"},{"AXA","EZE"},{"JFK","ADL"},{"EZE","AXA"},{"ANU","AXA"},{"AXA","JFK"},{"JFK","AUA"},{"AXA","EZE"},{"ADL","AXA"},{"AUA","ADL"},{"AUA","ADL"},{"AXA","AUA"},{"ADL","ANU"},{"ANU","AUA"},{"ADL","ANU"},{"JFK","ADL"},{"TIA","AUA"},{"EZE","AXA"},{"ANU","TIA"},{"AXA","ADL"},{"JFK","ANU"},{"ADL","ANU"},{"TIA","AUA"},{"ADL","ANU"},{"AUA","EZE"},{"JFK","ANU"},{"AUA","ANU"},{"ADL","AUA"},{"JFK","ADL"},{"TIA","JFK"},{"AXA","ANU"},{"AXA","ANU"},{"JFK","AXA"},{"AXA","JFK"},{"ANU","AXA"},{"ADL","EZE"},{"JFK","ANU"},{"JFK","AUA"},{"AXA","ANU"},{"JFK","TIA"},{"ANU","AXA"},{"JFK","AUA"},{"AXA","AUA"},{"ADL","EZE"},{"ADL","AXA"},{"JFK","TIA"},{"EZE","AUA"},{"AUA","ADL"},{"JFK","ADL"},{"EZE","TIA"},{"TIA","AXA"},{"AUA","ADL"},{"JFK","AUA"},{"JFK","AUA"},{"AUA","ANU"},{"JFK","ADL"},{"AXA","AUA"},{"EZE","JFK"},{"ANU","ADL"},{"ADL","EZE"},{"ANU","JFK"},{"EZE","ANU"},{"AUA","ANU"},{"JFK","EZE"},{"EZE","JFK"},{"AXA","AUA"},{"ANU","AUA"},{"TIA","JFK"},{"ANU","AXA"},{"TIA","ANU"},{"AUA","ADL"},{"ANU","AXA"},{"AXA","ADL"},{"AUA","AXA"},{"ADL","ANU"},{"EZE","AXA"},{"EZE","ANU"},{"AXA","EZE"},{"AUA","ADL"},{"AUA","ADL"},{"TIA","JFK"},{"JFK","AUA"},{"ADL","AUA"},{"AXA","TIA"},{"AXA","TIA"},{"AXA","ANU"},{"AUA","ADL"},{"AUA","AXA"},{"ADL","AUA"},{"ANU","TIA"},{"AUA","AXA"},{"ANU","TIA"},{"AXA","AUA"},{"EZE","JFK"},{"ADL","AUA"},{"ADL","EZE"},{"AXA","JFK"},{"JFK","TIA"},{"AUA","AXA"},{"AUA","AXA"},{"JFK","TIA"},{"EZE","ADL"},{"AUA","ADL"},{"ANU","AXA"},{"EZE","ADL"},{"ADL","ANU"},{"TIA","EZE"},{"ANU","AUA"},{"AUA","HBA"},{"JFK","AUA"},{"TIA","AUA"},{"TIA","ADL"},{"AXA","ADL"},{"EZE","AUA"},{"EZE","JFK"},{"ANU","TIA"},{"ANU","JFK"},{"EZE","AUA"},{"EZE","AUA"},{"ADL","JFK"},{"ADL","AUA"},{"ADL","AXA"},{"AXA","JFK"},{"AXA","AUA"},{"ANU","AUA"},{"ANU","JFK"},{"TIA","EZE"},{"ANU","AUA"},{"ADL","JFK"},{"JFK","ANU"},{"ADL","JFK"},{"ANU","AUA"},{"JFK","ADL"},{"AXA","ANU"},{"AXA","JFK"},{"JFK","ADL"},{"EZE","JFK"},{"TIA","AUA"},{"ANU","ADL"},{"AUA","AXA"},{"ADL","AXA"},{"AUA","JFK"},{"ADL","AXA"},{"EZE","ANU"},{"TIA","EZE"},{"EZE","JFK"},{"ADL","EZE"},{"ANU","JFK"},{"AUA","JFK"},{"ANU","JFK"},{"EZE","ADL"},{"AUA","ADL"},{"EZE","ANU"},{"AXA","EZE"},{"AXA","ADL"},{"JFK","TIA"},{"AUA","ADL"},{"EZE","TIA"},{"AXA","EZE"},{"EZE","AUA"},{"JFK","TIA"},{"AUA","AXA"},{"TIA","ANU"},{"AXA","JFK"},{"TIA","EZE"},{"EZE","ANU"},{"ANU","EZE"},{"JFK","TIA"},{"JFK","EZE"},{"TIA","AXA"},{"ANU","EZE"},{"AUA","TIA"},{"AXA","EZE"},{"ANU","AUA"},{"AUA","EZE"},{"AUA","EZE"},{"ADL","ANU"},{"AUA","AXA"},{"AXA","ADL"},{"JFK","EZE"}};
-    sol.findItinerary(tickets);
+    vector<vector<string>> tickets = {{"JFK","SFO"},{"JFK","ATL"},{"SFO","ATL"},{"ATL","JFK"},{"ATL","SFO"}};
+    sol.findItinerary1(tickets);
     sol.vecPrint();
     return 0;
 }
