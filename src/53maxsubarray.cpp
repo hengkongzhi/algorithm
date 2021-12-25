e9f12d518d19478ec3b2d410a9183bbf9c0e8cf0
cyang <1522207896@qq.com>
2021-12-26 00:26:51 +0800

    1226 最大子序列多种解法

--- src/53maxsubarray.cpp
+++ src/53maxsubarray.cpp
@@ -23,26 +23,55 @@ public:
         for (int i = 0; i < nums.size(); i++)
         {
             sum += nums[i];
+            if (sum < nums[i])
+            {
+                sum = nums[i];
+            }
             if (sum >= max)
             {
                 max = sum;
             }
-            if (sum < nums[i])
+        }
+        return max;
+    }
+    int maxSubArray1(vector<int>& nums)
+    {
+        int max = INT32_MIN;
+        int ret = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ret += nums[i];
+            if (ret > max)
             {
-                sum = nums[i];
-                if (sum >= max)
-                {
-                    max = sum;
-                }
+                max = ret;
+            }
+            if (ret < 0)
+            {
+                ret = 0;
             }
         }
         return max;
+    }
+    int maxSubArray2(vector<int>& nums)
+    {
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        int result = dp[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            if (dp[i] > result)
+            {
+                result = dp[i];
+            }
+        }
+        return result;
     }
 };
 int main()
 {
     Solution sol;
     vector<int> nums{-1,-2};
-    cout << sol.maxSubArray(nums) << endl;
+    cout << sol.maxSubArray2(nums) << endl;
     return 0;
 }
