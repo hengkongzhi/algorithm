462040154201f4395e69919e73f7e1fb85173d81
cyang <1522207896@qq.com>
2021-12-28 00:28:28 +0800

    1228 能否跳出数组

--- src/55canjump.cpp
+++ src/55canjump.cpp
@@ -82,11 +82,90 @@ public:
         }
         return true;
     }
+    bool deal(int val, vector<int>& myVec)
+    {
+        int count = 0;
+        int i = myVec.size() - 1;
+        for (; i >= 0; i--)
+        {
+            if (myVec[i] < val)
+            {
+                break;
+            }
+            count++;
+        }
+        if (count == 0)
+        {
+            return true;
+        }
+        auto iter = myVec.begin() + i + 1;
+        auto iter1 = myVec.end();
+        vector<int> newVec(iter, iter1);
+        myVec.clear();
+        myVec.assign(newVec.begin(), newVec.end());
+
+        return false;
+    }
+    bool canJump1(vector<int>& nums)
+    {
+        vector<int> myVec;
+        if (nums.size() == 0)
+        {
+            return true;
+        }
+        if (nums.size() == 1 && nums[0] == 0)
+        {
+            return true;
+        }
+        if (nums[0] == 0)
+        {
+            return false;
+        }
+        for (int i = 1; i < nums.size() - 1; i++)
+        {
+            if (nums[i] == 0)
+            {
+                myVec.push_back(i);
+            }
+        }
+        if (myVec.empty())
+        {
+            return true;
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != 0 && deal(i + nums[i], myVec))
+            {
+                return true;
+            }
+            else
+            {
+                if (nums[i] == 0)
+                {
+                    if (i >= myVec[0])
+                    {
+                        return false;
+                    }
+                }
+                else if (i + 1 < nums.size() - 1 &&
+                    nums[i + 1] == 0 &&
+                    myVec[0] <= i + 1)
+                {
+                    return false;
+                }
+            }
+        }
+        if (myVec.empty())
+        {
+            return true;
+        }
+        return false;
+    }
 };
 int main()
 {
     Solution sol;
     vector<int> nums{8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5};
-    cout << sol.canJump(nums) << endl;
+    cout << sol.canJump1(nums) << endl;
     return 0;
 }
