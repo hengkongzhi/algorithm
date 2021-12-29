04cdf4a3aaaf824611e180beea465a6b165208b2
cyang <1522207896@qq.com>
2021-12-30 00:23:53 +0800

    1230 跳跃游戏

--- src/45jump.cpp
+++ src/45jump.cpp
@@ -16,21 +16,71 @@ using namespace std;
 class Solution
 {
 public:
+    void deal(map<int, pair<int, int>>& myMap, int index, int cover, int& count)
+    {
+        for (auto iter = myMap.begin(); iter != myMap.end(); iter++)
+        {
+            if (index <= iter->second.first)
+            {
+                count = iter->second.second + 1;
+                break;
+            }
+        }
+        pair<int, int> myP(cover, count);
+        myMap.insert(pair<int, pair<int, int>>(index, myP));
+    }
     int jump(vector<int>& nums)
     {
+        if (nums.size() == 1)
+        {
+            return 0;
+        }
+        map<int, pair<int, int>> myMap;
         int lDistance = nums.size() - 1;
         int cover = 0;
-        int count = 0;
+        int count = 1;
         for (int i = 0; i <= cover; i++)
         {
             if (cover < i + nums[i])
             {
                 cover = i + nums[i];
+                deal(myMap, i, cover, count);
                 count++;
             }
             if (cover >= lDistance)
             {
                 break;
+            }
+        }
+        return count - 1;
+    }
+    int jump1(vector<int>& nums)
+    {
+        if (nums.size() == 1)
+        {
+            return 0;
+        }
+        int cur = 0;
+        int next = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            next = max(next, i + nums[i]);
+            if (i == cur)
+            {
+                if (i == nums.size() - 1)
+                {
+                    break;
+                }
+                else
+                {
+                    count++;
+                    cur = next;
+                    if (next >= nums.size() - 1)
+                    {
+                        break;
+                    }
+                }
             }
         }
         return count;
@@ -39,7 +89,7 @@ public:
 int main()
 {
     Solution sol;
-    vector<int> nums{8,2,4,4,4,9,5,2,5,8,8,0,8,6,9,1,1,6,3,5,1,2,6,6,0,4,8,6,0,3,2,8,7,6,5,1,7,0,3,4,8,3,5,9,0,4,0,1,0,5,9,2,0,7,0,2,1,0,8,2,5,1,2,3,9,7,4,7,0,0,1,8,5,6,7,5,1,9,9,3,5,0,7,5};
-    cout << sol.canJump2(nums) << endl;
+    vector<int> nums{3,1,2,3,1,2,3};
+    cout << sol.jump1(nums) << endl;
     return 0;
 }
